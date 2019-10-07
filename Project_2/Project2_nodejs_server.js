bd40e4e90838ec71570ca616b92496b345948bd6
Vatsal Sheth <vash3665@colorado.edu>
2019-10-06 19:20:22 -0600

    nodejs

--- Project_2/Project2_nodejs_server.js
+++ Project_2/Project2_nodejs_server.js
@@ -14,18 +14,6 @@ const wsServer = new WebSocketServer({
     httpServer: server
 });
 
-wsServer.on('request', function(request) {
-    const connection = request.accept(null, request.origin);
-
-    connection.on('message', function(message) {
-      console.log('Received Message:', message.utf8Data);
-      connection.sendUTF('Hi this is WebSocket server!');
-    });
-    connection.on('close', function(reasonCode, description) {
-        console.log('Client has disconnected.');
-    });
-});
-
 var con = mysql.createConnection({
   host: "localhost",
   user: USER,
@@ -35,10 +23,31 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
 	if (err) throw err;
-	
+	console.log("Connected!");
 }); 
 
-con.query("SELECT temperature FROM sensor_data7 ORDER BY id DESC LIMIT 10", function (err, result, fields) {
+wsServer.on('request', function(request) {
+    const connection = request.accept(null, request.origin);
+    
+    connection.on('message', function(message) {
+      if(message.utf8Data == "Refresh") {
+	con.query("SELECT * FROM sensor_data7 ORDER BY id DESC LIMIT 1", function (err, result, fields) {
 		if (err) throw err;
-		console.log(result);
+		var send_json = JSON.stringify(result);
+		connection.sendUTF(send_json);
 	});
+      }
+      else if(message.utf8Data == "Table") {
+	con.query("SELECT * FROM sensor_data7 ORDER BY id DESC LIMIT 10", function (err, result, fields) {
+		if (err) throw err;
+		var send_json = JSON.stringify(result);
+		connection.sendUTF(send_json);
+	});
+      }
+    });
+    //connection.on('close', function(reasonCode, description) {
+    //    console.log('Client has disconnected.');
+    //});
+});
+
+
