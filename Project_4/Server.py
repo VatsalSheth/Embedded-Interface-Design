64c5e0b9b7b0408d8de2131dcbf42e66ae212060
Vatsal Sheth <vash3665@colorado.edu>
2019-11-18 21:17:38 -0700

    added corrected

--- Project_4/Server.py
+++ Project_4/Server.py
@@ -1,33 +1,32 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtCore, QtWidgets, uic
 import sys
 
 class MainWindow(QtWidgets.QMainWindow):
-	
-	def __init__(self):
-		"""
+    
+    def __init__(self):
+        """
             __init__: This constructor searches al child objects created in GUI and links it with local variable defined in class 
         """
-		super(Login, self).__init__() # Call the inherited classes __init__ method
-		uic.loadUi('MainWindow.ui', self) # Load the .ui file
+        super(MainWindow, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi('MainWindow.ui', self) # Load the .ui file
 
 class Login(QtWidgets.QWidget):
     
     switch_window = QtCore.pyqtSignal()
         
-	def __init__(self):
-		"""
+    def __init__(self):
+        """
             __init__: This constructor searches al child objects created in GUI and links it with local variable defined in class 
         """
-		super(Login, self).__init__() # Call the inherited classes __init__ method
-		uic.loadUi('Login.ui', self) # Load the .ui file
-		self.button_login = self.findChild(QtWidgets.QPushButton, 'button_refresh') 
+        super(Login, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi('Login.ui', self) # Load the .ui file
+        self.button_login = self.findChild(QtWidgets.QPushButton, 'login_button') 
         self.button_login.clicked.connect(self.check_login)
-				
-	def check_login(self):
-		self.switch_window.emit()
+                
+    def check_login(self):
+        self.switch_window.emit()
 
 class Controller:
-
     def __init__(self):
         pass
 
